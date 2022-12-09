fixed the Sherman-Morrison-Woodbury formula for BFGS. Now matches results of finding the update by directly solving the linear system using the Hessian

BFGS.py
@@ -6,17 +6,17 @@ Created on Sat Nov 19 13:56:55 2022
 """
 
 import numpy as np
-from LU_factor import LU_factor
+# from LU_factor import LU_factor
+from numpy.linalg import norm
 
 def method(g, x, alpha, hessian, function, gradients):  # g is a list, not an array
 
     I = np.identity(len(x))
     
     if (method.iters == 0):
-        H = I
-        V = I
+        # H = I*norm(g)
+        V = I/norm(g)
     else:
-        H_old = method.H_old
         
         V_old = method.V_old
         g_old = method.g_old
@@ -25,20 +25,24 @@ def method(g, x, alpha, hessian, function, gradients):  # g is a list, not an ar
         y = np.array([i-j for (i,j) in zip(g,g_old)])
         s = np.array([i-j for (i,j) in zip(x,x_old)])
         
-        H = H_old + np.outer(y,y)/np.dot(y,s)-np.dot(np.outer(H_old.dot(s),s),H_old)/np.dot(s.dot(H_old),s)
+        # if calculating p using the Hessian directly 
+        # H_old = method.H_old
+        # H = H_old + np.outer(y,y)/np.dot(y,s)-np.dot(np.outer(H_old.dot(s),s),H_old)/np.dot(s.dot(H_old),s)
         
         sigma = 1/y.dot(s)
-        V = (I-sigma*np.outer(s,y))*V_old*(I-sigma*np.outer(y,s))+sigma*np.outer(s,s)
+        V = (I-sigma*np.outer(s,y))@V_old@(I-sigma*np.outer(y,s))+sigma*np.outer(s,s)
 
-    p = LU_factor(g, H)
-        
-    # p = np.dot(V,g)
+    # if calculating p using the Hessian directly (more computationally demanding)
+    # p = LU_factor(g, H)
+   
+    p = np.dot(V,g)
     
     p = [i*-1 for i in p]
     
     alpha = 1.0 # Newton step
-    
-    method.H_old = H
+
+    # store values for next iteration
+    # method.H_old = H
     method.V_old = V
     method.g_old = g
     method.x_old = x