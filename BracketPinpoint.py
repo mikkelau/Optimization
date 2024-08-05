revamped GoldenSectionSearch so that its pinpoint method is not recursive, it was having issues with stack overflow. Fixed a bug in BracketPinpoint

BracketPinpoint.py
@@ -12,7 +12,7 @@ import numpy as np
 from optimizer_linesearch import LineSearchOptimizer
 from numpy.linalg import norm
 
-def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds, lower_bounds, min_step): 
+def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds, lower_bounds, min_step, method_name): 
     # g is the gradient at current point
     # I pass in f_current to avoid another function eval
     
@@ -39,9 +39,13 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
         g2 = gradients(Xnew, function)
         g_eval = g2
         slope2 = np.dot(g2, p_dir) # this is the dot product
+        # see if bound enforcement caused step size to go below min_step
+        if norm(alpha2*p_dir) <= min_step and bounds_enforced: 
+            alpha = alpha2
+            break
         if (f2 > f_current+mu1*alpha2*slope_current) or (not first and (f2 > f1)): # new point is worse than current point
             # There is a point that satisfies strong wolfe conditions between current and guess, so find it
-            f_eval, g_eval, alpha = pinpoint(alpha1, alpha2, f_current, f1, f2, slope_current, slope1, slope2, mu1, mu2, function, gradients, X, p_dir, bounds_enforced, min_step)            
+            f_eval, g_eval, alpha = pinpoint(alpha1, alpha2, f_current, f1, f2, slope_current, slope1, slope2, mu1, mu2, function, gradients, X, p_dir, min_step)            
             break
         # otherwise, your guess point satisfies the first strong wolfe condition, so check if it satisfies the second
         if (abs(slope2) <= -1*mu2*slope_current): # if the slope at the guess location satisfies strong wolfe condition
@@ -49,7 +53,7 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
             break
         # otherwise, check which: you have overshot or undershot the point that you want
         elif (slope2 >= 0): # you have overshot the good point, so you know the good point exists between current point and guess
-            f_eval, g_eval, alpha = pinpoint(alpha2, alpha1, f_current, f2, f1, slope_current, slope2, slope1, mu1, mu2, function, gradients, X, p_dir, bounds_enforced, min_step)
+            f_eval, g_eval, alpha = pinpoint(alpha2, alpha1, f_current, f2, f1, slope_current, slope2, slope1, mu1, mu2, function, gradients, X, p_dir, min_step)
             break
         # how can it get stuck here? If alpha passed in is 0!
         else: # you are still moving downward at a high slope, extend your guess
@@ -65,7 +69,7 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
     
     return f_eval, g_eval, alpha
 
-def pinpoint(alpha_low, alpha_high, f_current, f_low, f_high, slope_current, slope_low, slope_high, mu1, mu2, function, gradients, X, p_dir, bounds_enforced, min_step):
+def pinpoint(alpha_low, alpha_high, f_current, f_low, f_high, slope_current, slope_low, slope_high, mu1, mu2, function, gradients, X, p_dir, min_step):
     k = 0
     
     while True:
@@ -74,10 +78,7 @@ def pinpoint(alpha_low, alpha_high, f_current, f_low, f_high, slope_current, slo
         if norm(max(alpha_low,alpha_high)*p_dir) < min_step:
             print("minimum step enforced")
             minimum_step_enforced = True
-            if bounds_enforced:
-                alpha_p = max(alpha_low,alpha_high)
-            else:
-                alpha_p = min_step/norm(p_dir)
+            alpha_p = min_step/norm(p_dir)
         else:
             alpha_p = interpolate(alpha_low, alpha_high, f_low, f_high, slope_low, slope_high)
             if norm(alpha_p*p_dir) < min_step:

GoldenSectionSearch.py
@@ -7,8 +7,9 @@ Created on Fri Oct 27 08:57:52 2023
 
 import numpy as np
 from optimizer_linesearch import LineSearchOptimizer
+from numpy.linalg import norm
 
-def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds, lower_bounds): 
+def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds, lower_bounds, min_step, method_name): 
     # g is the gradient at current point
     # I pass in f_current to avoid another function eval
     
@@ -34,9 +35,13 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
         g4 = gradients(Xnew, function)
         g_eval = g4
         slope4 = np.dot(g4, p_dir) # this is the dot product
+        # see if bound enforcement caused step size to go below min_step
+        if norm(alpha4*p_dir) <= min_step and bounds_enforced: 
+            alpha = alpha4
+            break
         if (f4 > f_current+mu1*alpha4*slope_current) or (not first and (f4 > f1)): # new point is worse than current point
             # There is a point that satisfies strong wolfe conditions between current and guess, so find it
-            f_eval, g_eval, alpha = pinpoint(alpha1, alpha4, f_current, f1, f4, slope_current, mu1, mu2, function, gradients, X, p_dir)            
+            f_eval, g_eval, alpha = pinpoint(alpha1, alpha4, f_current, slope_current, mu1, mu2, function, gradients, X, p_dir, min_step)            
             break
         # otherwise, your guess point satisfies the first strong wolfe condition, so check if it satisfies the second
         if (abs(slope4) <= -1*mu2*slope_current): # if the slope at the guess location satisfies strong wolfe condition
@@ -44,7 +49,7 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
             break
         # otherwise, check which: you have overshot or undershot the point that you want
         elif (slope4 >= 0): # you have overshot the good point, so you know the good point exists between current point and guess
-            f_eval, g_eval, alpha = pinpoint(alpha1, alpha4, f_current, f1, f4, slope_current, mu1, mu2, function, gradients, X, p_dir)
+            f_eval, g_eval, alpha = pinpoint(alpha1, alpha4, f_current, slope_current, mu1, mu2, function, gradients, X, p_dir, min_step)
             break
         # how can it get stuck here? If alpha passed in is 0!
         else: # you are still moving downward at a high slope, extend your guess
@@ -60,10 +65,20 @@ def linesearch(f_current, function, g, gradients, X, p_dir, alpha, upper_bounds,
     
     return f_eval, g_eval, alpha
 
-def pinpoint(alpha1, alpha4, f_current, f1, f4, slope_current, mu1, mu2, function, gradients, X, p_dir, alpha2=None, f2=None, alpha3=None, f3=None):
+def pinpoint(alpha1, alpha4, f_current, slope_current, mu1, mu2, function, gradients, X, p_dir, min_step):
     phi = (1+5**0.5)/2
+    k = 0
+    alpha2 = None
+    alpha3 = None
     while True:
-        # add a check to see if the interval is already too small? let's see if it's needed first
+        # enforce minimum step on upper alpha. Sometimes the step can get too small, especially when using estimations of the gradient
+        if norm(alpha4*p_dir) < min_step:
+            # print("minimum step enforced")
+            alpha_p = min_step/norm(p_dir)
+            Xnew = X+alpha_p*p_dir
+            f_p = function(Xnew)
+            g_p = gradients(Xnew, function)
+            break
         
         if alpha2==None:
             # calulate first interior point
@@ -96,18 +111,44 @@ def pinpoint(alpha1, alpha4, f_current, f1, f4, slope_current, mu1, mu2, functio
                     alpha_p = alpha3
                     break # nailed it
         
-        # enforce minimum step. Sometimes the step can get too small, especially when using estimations of the gradient
-        if (max(alpha4,alpha1) < (np.finfo(np.float32).eps)):
+        # if too many tries in the loop, just take the best of what you have.
+        if k > 6:
             if (f2>f3):
                 X3 = X+alpha3*p_dir
-                return f3, gradients(X3, function), alpha3
+                f_p = f3
+                g_p = gradients(X3, function)
+                alpha_p = alpha3
+                break
             else:
                 X2 = X+alpha2*p_dir
-                return f2, gradients(X2, function), alpha2
-            
+                f_p = f2
+                g_p = gradients(X2, function)
+                alpha_p = alpha2
+                break
+        
+        # shrink the window
         if (f2>f3):
-            return pinpoint(alpha2, alpha4, f_current, f2, f4, slope_current,  mu1, mu2, function, gradients, X, p_dir, alpha2=alpha3, f2=f3)
+            alpha1 = alpha2
+            alpha2 = alpha3
+            f2 = f3
+            alpha3 = None
+            f3 = None
         else:
-            return pinpoint(alpha1, alpha3, f_current, f1, f3, slope_current,  mu1, mu2, function, gradients, X, p_dir, alpha3=alpha2, f3=f2)
+            alpha4 = alpha3
+            alpha3 = alpha2
+            f3 = f2
+            alpha2 = None
+            f2 = None
+            
+        # increment counter
+        k +=1
     
+    # enforce minimum step on alpha_p
+    if norm(alpha_p*p_dir) < min_step:
+        # print("minimum step enforced")
+        alpha_p = min_step/norm(p_dir)
+        Xnew = X+alpha_p*p_dir
+        f_p = function(Xnew)
+        g_p = gradients(Xnew, function)
+        
     return f_p, g_p, alpha_p 