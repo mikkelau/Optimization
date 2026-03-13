7667af4f6e9f3eec11b55493781f3a3ddf016e40
Mikkel Unrau <mikkelaunrau@gmail.com>
2026-03-12 22:01:57 -0600

    broke out a child class of the Genetic Algorithm Optimizer that handles real encoded variables

--- GA_Tester.py
+++ GA_Tester.py
@@ -5,13 +5,13 @@ Created on Sun Jun  9 12:27:17 2024
 @author: mikke
 """
 
-from optimizer_GA import GeneticAlgorithmOptimizer 
+from optimizer_RealEncodedGA import RealEncodedGAOptimizer 
 
 # from BeanFunction import function, upper_bounds, lower_bounds, gradients, hessian, f_opt, f_opt_tol
 # from Brachistochrone import function, upper_bounds, lower_bounds
 # from Rosenbrock import function, upper_bounds, lower_bounds, gradients, hessian
 # from GoldsteinPrice import function, upper_bounds, lower_bounds, gradients, hessian # has local minima
-from TwoSpring import function, upper_bounds, lower_bounds, gradients, hessian
+from TwoSpring import function, upper_bounds, lower_bounds
 # from Rosenbrock_Stretched import function, upper_bounds, lower_bounds, gradients, hessian
 # from BoothFunction import function, upper_bounds, lower_bounds, gradients, hessian
 # from BukinFunction import function, upper_bounds, lower_bounds, gradients, hessian
@@ -29,7 +29,7 @@ for runNum in range(numRuns):
     # initial guess
 
     # initialize the optimizer
-    optimizer = GeneticAlgorithmOptimizer(function, upper_bounds, lower_bounds, max_iters, plot_generations=True, num_pops=33)
+    optimizer = RealEncodedGAOptimizer(function, upper_bounds, lower_bounds, max_iters, plot_generations=True, num_pops=33)
     
     # call optimize
     optimizer.optimize()
--- optimizer_GA.py
+++ optimizer_GA.py
@@ -7,131 +7,34 @@ Created on Fri Jun  7 11:43:43 2024
 
 import optimizer
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import qmc
 import random
 import time
+import matplotlib.pyplot as plt
 
 class GeneticAlgorithmOptimizer(optimizer.Optimizer):
-    def __init__(self, function, upper_bounds, lower_bounds, max_iters, plot_generations=False, num_pops=None):
+    def __init__(self, function, upper_bounds, lower_bounds, max_iters, num_pops=None):
         super().__init__(function, upper_bounds, lower_bounds, max_iters)
         self.x_list = []
         self.f_list = []
-        self.plot_generations = plot_generations
-        self.num_pops = num_pops
-        
-    def contour_plot(self,points=None):
-        if len(self.upper_bounds) == 2:
-            if points is None:
-                points = self.x_list
-            # enable interactive mode
-            plt.ion()
-            fig = self.make_contour_plot(self.function, self.upper_bounds, self.lower_bounds)
-            # plot the points that got passed in
-            plt.plot([i[0] for i in points],[i[1] for i in points],c='red',marker='o',markerfacecolor='none')
-            return fig
-        else:
-            print("Cannot create contour plot. Number of independent variables needs to be two.\n") 
-    
-    # based on NSGA technique
-    def create_children(self,pool,fitness):
-
-        n = len(self.upper_bounds)
-        ratio = 1.2 # scalar. If it lies in the range [0, 1], the children created are within the two parents. If algorithm is premature, try to set ratio larger than 1.0
-        crossfrac = 2/n #default in NSGA code
-        
-        # generate offspring
-        children = np.empty((0,n))
-        # create parents
-        for i in range(0,len(pool),2):
-            mom = pool[i]
-            dad = pool[i+1]
-            crossover_flag = [random.random() < crossfrac for j in range(len(mom))]
-            randNum = np.array([random.random() for j in range(len(mom))])
-                        
-            # crossover
-            child1 = mom+crossover_flag*randNum*ratio*(dad-mom)
-            child2 = dad-crossover_flag*randNum*ratio*(dad-mom)
-            
-            children = np.vstack((children,np.vstack((child1,child2))))
-        
-        return children
-    
-    # using the golden ratio
-    # def create_children(self,pool,fitness):
-
-    #     phi = (1+5**0.5)/2
-        
-    #     # generate offspring
-    #     children = np.empty((0,2))
-    #     # create parents
-    #     for i in range(0,len(pool),2):
-    #         if fitness[i] < fitness[i+1]:
-    #             parent1 = pool[i]
-    #             parent2 = pool[i+1]
-    #         else:
-    #             parent1 = pool[i+1]
-    #             parent2 = pool[i]
-                        
-    #         # crossover
-    #         child1 = parent1+(parent2-parent1)/(1+phi)
-    #         child2 = parent1-(parent2-parent1)/(1+phi)
-
-    #         children = np.vstack((children,np.vstack((child1,child2))))
-        
-    #     return children
-    
-    def mutation(self, children, gen):
-        max_iters = self.max_iters
-        upper_bounds = self.upper_bounds
-        lower_bounds = self.lower_bounds
-        n = len(upper_bounds)
-        
-        # mutation parameters
-        scale = 0.1 #determines the standard deviation of the random numbers generated
-        shrink = 0.5 #scalar, [0,1]. As the optimization progress goes forward, decrease the mutation range (for example, shrink?[0.5, 1.0]) is usually used for local search
-        mutfrac =2/n # =crossfrac , default in NSGA code
-                
-        # calculate the mutation parameters using scale and shrink
-        S = np.array([scale*(1-shrink*gen/max_iters)*(upper_bounds[n]-lower_bounds[n]) for n in range(n)])
-        mutation_flag = [random.random() < mutfrac for i in range(n)]
-        # do the mutation
-        for i in range(len(children)):
-            randNum = np.array([np.random.randn() for j in range(n)])
-            children[i] += S*mutation_flag*randNum
-        # enforce bounds
-        children = np.clip(children, lower_bounds, upper_bounds)    
-        
-        return children
-        
-    def optimize(self):
-        function = self.function
-        max_iters = self.max_iters
-        upper_bounds = self.upper_bounds
-        lower_bounds = self.lower_bounds
-        num_pops = self.num_pops
-                
-        n = len(upper_bounds)
         
         # determine population size
+        n = len(self.upper_bounds)
         if num_pops:
             #needs to be even
             if (num_pops%2 == 1):
                 num_pops+=1
         else:
-            num_pops = 16*n # should be 15-20 x number of design variables
+            num_pops = 16*n # should be 15-20 x number of design variables       
+        self.num_pops = num_pops
         
-        # sample the solution space
-        engine = qmc.LatinHypercube(d=n)
-        sample = engine.random(n=num_pops)
+    def optimize(self):
+        function = self.function
+        max_iters = self.max_iters
+        upper_bounds = self.upper_bounds
+        num_pops = self.num_pops
+        n = len(upper_bounds)        
         
-        # if binary == True:
-        #     threshold = 0.5 # 0.5 for equal probability of 0 or 1
-        #     points = (sample >= threshold).astype(int)
-        # else:
-        #     points = qmc.scale(sample, lower_bounds, upper_bounds)
-            
-        points = qmc.scale(sample, lower_bounds, upper_bounds)
+        points = self.initialize_pops()
         
         # plot initial population
         if self.plot_generations and n==2:
